2582a4828dec5cf36a84ca558b85dad6461c5faf
MrKnight2916 <eamadorencarnacion@byupathway.edu>
2025-09-27 17:23:23 -0400

    Update project files

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,5 +1,5 @@
 // --------------------
-// Datos de templos
+// Temple Data
 // --------------------
 const temples = [
   { templeName: "Aba Nigeria", location: "Aba, Nigeria", dedicated: "2005, August, 7", area: 11500, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg" },
@@ -9,20 +9,20 @@ const temples = [
   { templeName: "Washington D.C.", location: "Kensington, Maryland, United States", dedicated: "1974, November, 19", area: 156558, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg" },
   { templeName: "Lima Perú", location: "Lima, Perú", dedicated: "1986, January, 10", area: 9600, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg" },
   { templeName: "Mexico City Mexico", location: "Mexico City, Mexico", dedicated: "1983, December, 2", area: 116642, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg" },
-  // Temples adicionales
+  // Additional Temples
   { templeName: "Santo Domingo", location: "Santo Domingo, Dominican Republic", dedicated: "2002, April, 24", area: 17600, imageUrl: "images/santo_domingo_dominican_republic_temple_lds.jpeg" },
   { templeName: "Salt Lake Temple", location: "Salt Lake City, Utah, United States", dedicated: "1893, April, 6", area: 253000, imageUrl: "images/salt_lake_temple_lds.jpeg" },
   { templeName: "San Juan", location: "San Juan, Puerto Rico", dedicated: "1983, August, 27", area: 16500, imageUrl: "images/san_juan_puerto_rico_temple_exterior.jpeg" }
 ];
 
 // --------------------
-// Footer dinámico
+// Dynamic Footer
 // --------------------
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = "Last modified: " + document.lastModified;
 
 // --------------------
-// Función para crear tarjetas
+// Card Creation Feature
 // --------------------
 const gallery = document.querySelector('.gallery');
 
@@ -42,12 +42,11 @@ function displayTemples(filteredTemples) {
         gallery.appendChild(fig);
     });
 }
-
-// Mostrar todos al inicio
+// Initial display of all temples
 displayTemples(temples);
 
 // --------------------
-// Filtrado de templos
+// Filtering Feature
 // --------------------
 const navLinks = document.querySelectorAll('#main-nav a');
 navLinks.forEach(link => {
